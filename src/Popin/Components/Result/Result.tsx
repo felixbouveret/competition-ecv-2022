2236407413e577c3efb6f6321de635221b6f53e3
Estelle <estelle.denage@gmail.com>
2022-06-23 22:57:54 +0200

    desktop display

--- src/Popin/Components/Result/Result.tsx
+++ src/Popin/Components/Result/Result.tsx
@@ -6,38 +6,40 @@ import ProfileChart from "../ProfileChart";
 export default function Result() {
   return (
     <div className="result">
-      <p className="log">
-        Pour enregistrer vos recommandations, pensez à vous
-        <span>connecter</span> ou vous <span>inscrire</span>.
-      </p>
       <section className="reco">
-        <h1 className="title">Voici les vins que je vous recommande</h1>
-        <div className="graphsContainer">
-          <div className="cards">
-            <ResultCard
-              tags={["Fort", "Boisé", "Épicé"]}
-              title="Cuvée tradition 2018"
-              compatibility="80"
-              price={19.99}
-              image="/assets/img/wine-glass.png"
-              shop="le puits d'amour"
-            />
-            <ResultCard
-              tags={["Fort", "Boisé", "Fruité"]}
-              title="Cuvée spéciale 2016"
-              compatibility="88"
-              price={12.99}
-              image="/assets/img/wine-glass-2.jpg"
-              shop="le puits d'amour"
-            />
-            <ResultCard
-              tags={["Tanique", "Fumé", "Épicé"]}
-              title="Cuvée primeur 2017"
-              compatibility="75"
-              price={9.99}
-              image="/assets/img/wine-glass.png"
-              shop="le Chateau Dillon"
-            />
+        <p className="log">
+          Pour enregistrer vos recommandations, pensez à vous
+          <span> connecter</span> ou vous <span>inscrire</span>.
+        </p>
+        <div>
+          <h1 className="title">Voici les vins que je vous recommande</h1>
+          <div className="graphsContainer">
+            <div className="cards">
+              <ResultCard
+                tags={["Fort", "Boisé", "Épicé"]}
+                title="Cuvée tradition 2018"
+                compatibility="80"
+                price={19.99}
+                image="/assets/img/wine-glass.png"
+                shop="le puits d'amour"
+              />
+              <ResultCard
+                tags={["Fort", "Boisé", "Fruité"]}
+                title="Cuvée spéciale 2016"
+                compatibility="88"
+                price={12.99}
+                image="/assets/img/wine-glass-2.jpg"
+                shop="le puits d'amour"
+              />
+              <ResultCard
+                tags={["Tanique", "Fumé", "Épicé"]}
+                title="Cuvée primeur 2017"
+                compatibility="75"
+                price={9.99}
+                image="/assets/img/wine-glass.png"
+                shop="le Chateau Dillon"
+              />
+            </div>
           </div>
         </div>
       </section>
